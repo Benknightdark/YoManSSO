a704d632316e4aa6d6431d6f93b4c2dac57cd8d4
moon791017 <moon791017@gmail.com>
2020-02-29 15:38:07 +0800

    google service add get userprofile

---
+++ Models/Google/GoogleUserProfile.cs
@@ -0,0 +1,13 @@
+namespace OAthLib.Models.Google {
+
+    public class GoogleUserProfile {
+        public string id { get; set; }
+        public string email { get; set; }
+        public bool verified_email { get; set; }
+        public string name { get; set; }
+        public string given_name { get; set; }
+        public string family_name { get; set; }
+        public string picture { get; set; }
+        public string locale { get; set; }
+    }
+}
--- Services/GoogleService.cs
+++ Services/GoogleService.cs
@@ -1,33 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using OAthLib.Models.Google;
 using OAthLib.Services.Helpers;
 
-namespace OAthLib.Services
-{
-    public class GoogleService
-    {
+namespace OAthLib.Services {
+    public class GoogleService {
         private HttpClient Client { get; set; }
         private ConfigService _config { get; }
-        public GoogleService(HttpClient client, ConfigService config)
-        {
+        public GoogleService (HttpClient client, ConfigService config) {
             _config = config;
             // client.BaseAddress = new System.Uri("https://oauth2.googleapis.com");
             Client = client;
         }
-         /// <summary>
+        /// <summary>
         /// 取得 google 登入頁面網址
         /// </summary>
         /// <returns></returns>
-        public async Task<string> GetAccessUrl()
-        {
-            await Task.Yield();
-            var googleConfig =  _config.GetGoogleConfig();
+        public async Task<string> GetAccessUrl () {
+            await Task.Yield ();
+            var googleConfig = _config.GetGoogleConfig ();
             string AccessUrl = $"https://accounts.google.com/o/oauth2/v2/auth";
-            AccessUrl +="?response_type=code";
+            AccessUrl += "?response_type=code";
             AccessUrl += $"&client_id={googleConfig.ClientID}";
             AccessUrl += $"&redirect_uri={googleConfig.RedirectUrl}";
             AccessUrl += "&scope=email openid profile";
@@ -40,26 +38,39 @@ namespace OAthLib.Services
         /// </summary>
         /// <param name="Code">授權碼</param>
         /// <returns></returns>
-        public async Task<GoogleAccessToken> GetAccessToken(string Code)
-        {
-            var googleConfigData = _config.GetGoogleConfig();
-         
-          var nvc = new List<KeyValuePair<string, string>>();
-            nvc.Add(new KeyValuePair<string, string>("grant_type", "authorization_code"));
-            nvc.Add(new KeyValuePair<string, string>("code", Code));
-            nvc.Add(new KeyValuePair<string, string>("redirect_uri", googleConfigData.RedirectUrl));
-            nvc.Add(new KeyValuePair<string, string>("client_id", googleConfigData.ClientID));
-            nvc.Add(new KeyValuePair<string, string>("client_secret", googleConfigData.ClientSecret));
+        public async Task<GoogleAccessToken> GetAccessToken (string Code) {
+            var googleConfigData = _config.GetGoogleConfig ();
 
-            var req = await Client.PostAsync("https://oauth2.googleapis.com/token", new FormUrlEncodedContent(nvc));
-            try
-            {
-                var data = await req.Content.ReadAsStringAsync();
-                 var resData = JsonConvert.DeserializeObject<GoogleAccessToken>(data);
+            var nvc = new List<KeyValuePair<string, string>> ();
+            nvc.Add (new KeyValuePair<string, string> ("grant_type", "authorization_code"));
+            nvc.Add (new KeyValuePair<string, string> ("code", Code));
+            nvc.Add (new KeyValuePair<string, string> ("redirect_uri", googleConfigData.RedirectUrl));
+            nvc.Add (new KeyValuePair<string, string> ("client_id", googleConfigData.ClientID));
+            nvc.Add (new KeyValuePair<string, string> ("client_secret", googleConfigData.ClientSecret));
+
+            var req = await Client.PostAsync ("https://oauth2.googleapis.com/token", new FormUrlEncodedContent (nvc));
+            try {
+                var data = await req.Content.ReadAsStringAsync ();
+                var resData = JsonConvert.DeserializeObject<GoogleAccessToken> (data);
                 return resData;
+            } catch (System.Exception e) {
+                throw e;
             }
-            catch (System.Exception e)
-            {
+        }
+
+        /// <summary>
+        /// 取得Google 使用者profile
+        /// </summary>
+        /// <param name="lineAccessToken">AccessToken</param>
+        /// <returns></returns>
+        public async Task<GoogleUserProfile> GetUserProfile (GoogleAccessToken googleAccessToken) {
+            try {
+                var req = await Client.GetAsync ($"https://www.googleapis.com/oauth2/v1/userinfo?access_token={googleAccessToken.access_token}");
+                var data = await req.Content.ReadAsStringAsync ();
+                var resData = JsonConvert.DeserializeObject<GoogleUserProfile> (data);
+                return resData;
+
+            } catch (Exception e) {
                 throw e;
             }
         }
