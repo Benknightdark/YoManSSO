bad7c485f7f08b32760b4820cb733823c097a09f
moon791017 <moon791017@gmail.com>
2020-02-29 13:37:07 +0800

    OK

--- Services/GoogleService.cs
+++ Services/GoogleService.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using OAthLib.Services.Helpers;
 
 namespace OAthLib.Services
@@ -10,8 +12,25 @@ namespace OAthLib.Services
         public GoogleService(HttpClient client, ConfigService config)
         {
             _config = config;
-            client.BaseAddress = new System.Uri("");
+          //  client.BaseAddress = new System.Uri("");
             Client = client;
+        }
+         /// <summary>
+        /// 取得 google 登入頁面網址
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetAccessUrl()
+        {
+            await Task.Yield();
+            var googleConfig =  _config.GetGoogleConfig();
+            string AccessUrl = $"https://accounts.google.com/o/oauth2/v2/auth";
+            AccessUrl +="?response_type=code";
+            AccessUrl += $"&client_id={googleConfig.ClientID}";
+            AccessUrl += $"&redirect_uri={googleConfig.RedirectUrl}";
+            AccessUrl += "&scope=email openid";
+            // AccessUrl += $"&state={Guid.NewGuid()}";
+            return (AccessUrl);
+
         }
     }
 }
